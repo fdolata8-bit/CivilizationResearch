1bb655c94f29dfe0a798b891d5620fa370468f4c
fdolata8-bit <fdolata8@gmail.com>
2026-06-18 01:46:25 +0200

    Update agent.py

--- engine/agent.py
+++ engine/agent.py
@@ -1,29 +1,7 @@
-import random
-
-
 class Agent:
-    def __init__(self, imie):
-        self.imie = imie
-        self.iq = random.randint(80, 140)
-        self.wiedza = 0
-        self.ciekawosc = random.randint(1, 100)
+    def __init__(self):
+        self.inteligencja = 0.1
 
-    def ucz_sie(self):
-        przyrost = max(1, self.iq // 50)
-        self.wiedza += przyrost
-
-        print(
-            self.imie,
-            "IQ:",
-            self.iq,
-            "Wiedza:",
-            self.wiedza
-        )
-
-    def czy_odkryl_cos(self):
-        szansa = self.ciekawosc + (self.iq // 2)
-
-        if random.randint(1, 1000) < szansa:
-            return True
-
-        return False
+    def rozwoj(self):
+        self.inteligencja += 0.002
+            
