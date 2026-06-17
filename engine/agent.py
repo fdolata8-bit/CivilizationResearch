e44242857457dff6981f9296fa3f407504d92b73
fdolata8-bit <fdolata8@gmail.com>
2026-06-18 01:59:21 +0200

    Update agent.py

--- engine/agent.py
+++ engine/agent.py
@@ -1,7 +1,8 @@
 class Agent:
     def __init__(self):
         self.inteligencja = 0.1
+        self.doswiadczenie = 0
 
     def rozwoj(self):
+        self.doswiadczenie += 1
         self.inteligencja += 0.002
-            
