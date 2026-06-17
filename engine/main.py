35a21e9197e13a537bb020e8b235b13a913aee9b
fdolata8-bit <fdolata8@gmail.com>
2026-06-18 01:10:20 +0200

    Update main.py

--- engine/main.py
+++ engine/main.py
@@ -1,2 +1,16 @@
-print("Civilization Research Engine")
-print("Symulacja uruchomiona")
+from agent import Agent
+from swiat import Swiat
+
+
+swiat = Swiat()
+
+adam = Agent("Adam")
+ewa = Agent("Ewa")
+
+swiat.dodaj_agenta(adam)
+swiat.dodaj_agenta(ewa)
+
+for _ in range(10):
+    swiat.wykonaj_ture()
+
+swiat.baza_wiedzy.pokaz_wynalazki()
